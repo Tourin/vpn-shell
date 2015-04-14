5d2ee46e90acf01738813b38555f3fbe00cbc1f9
tourin <tanglin5618@gmail.com>
2015-04-14 13:18:39 +0800

    Update centos6_install_shadowsocks.sh

--- centos6_install_shadowsocks.sh
+++ centos6_install_shadowsocks.sh
@@ -2,23 +2,43 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 #===============================================================================================
-#   System Required:  CentOS6.x (32bit/64bit)
-#   Description:  Install Shadowsocks(libev) for CentOS
+#   System Required:  CentOS 6,7, Debian, Ubuntu
+#   Description:  One click Install Shadowsocks(Python)
+#   Author: Teddysun <i@teddysun.com>
+#   Intro:  http://teddysun.com/342.html
 #===============================================================================================
 
 clear
+echo ""
 echo "#############################################################"
-echo "# Install Shadowsocks(libev) for CentOS 6 or 7 (32bit/64bit)"
+echo "# One click Install Shadowsocks(Python)"
+echo "# Intro: http://teddysun.com/342.html"
+echo "#"
+echo "# Author: Teddysun <i@teddysun.com>"
 echo "#"
 echo "#############################################################"
 echo ""
 
 # Make sure only root can run our script
 function rootness(){
-if [[ $EUID -ne 0 ]]; then
-   echo "Error:This script must be run as root!" 1>&2
-   exit 1
-fi
+    if [[ $EUID -ne 0 ]]; then
+       echo "Error:This script must be run as root!" 1>&2
+       exit 1
+    fi
+}
+
+# Check OS
+function checkos(){
+    if [ -f /etc/redhat-release ];then
+        OS=CentOS
+    elif [ ! -z "`cat /etc/issue | grep bian`" ];then
+        OS=Debian
+    elif [ ! -z "`cat /etc/issue | grep Ubuntu`" ];then
+        OS=Ubuntu
+    else
+        echo "Not support OS, Please reinstall OS and retry!"
+        exit 1
+    fi
 }
 
 # Get version
@@ -54,11 +74,11 @@ fi
 function pre_install(){
     # Not support CentOS 5
     if centosversion 5; then
-        echo "Not support CentOS 5, please change to CentOS 6 or 7 and try again."
+        echo "Not support CentOS 5.x, please change to CentOS 6,7 or Debian or Ubuntu and try again."
         exit 1
     fi
-    #Set shadowsocks-libev config password
-    echo "Please input password for shadowsocks-libev:"
+    #Set shadowsocks config password
+    echo "Please input password for shadowsocks:"
     read -p "(Default password: teddysun.com):" shadowsockspwd
     if [ "$shadowsockspwd" = "" ]; then
         shadowsockspwd="teddysun.com"
@@ -78,13 +98,18 @@ function pre_install(){
     echo "Press any key to start...or Press Ctrl+C to cancel"
     char=`get_char`
     #Install necessary dependencies
-    yum install -y wget unzip openssl-devel gcc swig python python-devel python-setuptools autoconf libtool libevent
-    yum install -y automake make curl curl-devel zlib-devel openssl-devel perl perl-devel cpio expat-devel gettext-devel
+    if [ "$OS" == 'CentOS' ]; then
+        yum install -y wget unzip openssl-devel gcc swig python python-devel python-setuptools autoconf libtool libevent
+        yum install -y automake make curl curl-devel zlib-devel perl perl-devel cpio expat-devel gettext-devel
+    else
+        apt-get -y update
+        apt-get -y install python python-dev python-pip curl wget unzip gcc swig automake make perl cpio
+    fi
     # Get IP address
     echo "Getting Public IP address, Please wait a moment..."
-    IP=`curl -s checkip.dyndns.com | cut -d' ' -f 6  | cut -d'<' -f 1`
-    if [ $? -ne 0 -o -z $IP ]; then
-        IP=`curl -s -4 ipinfo.io | grep "ip" | awk -F\" '{print $4}'`
+    IP=$(curl -s -4 icanhazip.com)
+    if [[ "$IP" = "" ]]; then
+        IP=$(curl -s -4 ipinfo.io | grep "ip" | awk -F\" '{print $4}')
     fi
     echo -e "Your main public IP is\t\033[32m$IP\033[0m"
     echo ""
@@ -93,45 +118,38 @@ function pre_install(){
     cd $cur_dir
 }
 
-# Download latest shadowsocks-libev
+# Download files
 function download_files(){
-    if [ -f shadowsocks-libev.zip ];then
-        echo "shadowsocks-libev.zip [found]"
-    else
-        if ! wget --no-check-certificate https://github.com/shadowsocks/shadowsocks-libev/archive/master.zip -O shadowsocks-libev.zip;then
-            echo "Failed to download shadowsocks-libev.zip"
+    if [ "$OS" == 'CentOS' ]; then
+        if ! wget --no-check-certificate -O ez_setup.py https://bootstrap.pypa.io/ez_setup.py; then
+            echo "Failed to download ez_setup.py!"
             exit 1
         fi
-    fi
-    unzip shadowsocks-libev.zip
-    if [ $? -eq 0 ];then
-        cd $cur_dir/shadowsocks-libev-master/
+        # Download shadowsocks chkconfig file
+        if ! wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks -O /etc/init.d/shadowsocks; then
+            echo "Failed to download shadowsocks chkconfig file!"
+            exit 1
+        fi
     else
-        echo ""
-        echo "Unzip shadowsocks-libev failed! Please visit http://teddysun.com/357.html and contact."
-        exit 1
-    fi
-    # Download start script
-    if ! wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-libev; then
-        echo "Failed to download shadowsocks-libev start script!"
-        exit 1
+        if ! wget --no-check-certificate https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-debian -O /etc/init.d/shadowsocks; then
+            echo "Failed to download shadowsocks chkconfig file!"
+            exit 1
+        fi
     fi
 }
 
 # Config shadowsocks
 function config_shadowsocks(){
-    if [ ! -d /etc/shadowsocks-libev ];then
-        mkdir /etc/shadowsocks-libev
-    fi
-    cat > /etc/shadowsocks-libev/config.json<<-EOF
+    cat > /etc/shadowsocks.json<<-EOF
 {
-    "server":"${IP}",
+    "server":"0.0.0.0",
     "server_port":8989,
     "local_address":"127.0.0.1",
     "local_port":1080,
     "password":"${shadowsockspwd}",
-    "timeout":600,
-    "method":"aes-256-cfb"
+    "timeout":300,
+    "method":"aes-256-cfb",
+    "fast_open":false
 }
 EOF
 }
@@ -143,7 +161,7 @@ function iptables_set(){
     if [ $? -eq 0 ]; then
         /etc/init.d/iptables status | grep '8989' | grep 'ACCEPT' >/dev/null 2>&1
         if [ $? -ne 0 ]; then
-            /sbin/iptables -A INPUT -m state --state NEW -m tcp -p tcp --dport 8989 -j ACCEPT
+            /sbin/iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 8989 -j ACCEPT
             /etc/init.d/iptables save
             /etc/init.d/iptables restart
         else
@@ -154,98 +172,105 @@ function iptables_set(){
     fi
 }
 
-# Install 
-function install(){
-    # Build and Install shadowsocks-libev
-    if [ -s /usr/local/bin/ss-server ];then
-        echo "shadowsocks-libev has been installed!"
-        exit 0
-    else
-        ./configure
-        make && make install
-        if [ $? -eq 0 ]; then
-            mv $cur_dir/shadowsocks-libev-master/shadowsocks-libev /etc/init.d/shadowsocks
-            chmod +x /etc/init.d/shadowsocks
-            # Add run on system start up
-            chkconfig --add shadowsocks
-            chkconfig shadowsocks on
-            # Start shadowsocks
-            /etc/init.d/shadowsocks start
-            if [ $? -eq 0 ]; then
-                echo "Shadowsocks-libev start success!"
-            else
-                echo "Shadowsocks-libev start failure!"
-            fi
-        else
-            echo ""
-            echo "Shadowsocks-libev install failed! Please visit http://teddysun.com/357.html and contact."
-            exit 1
+# Install Shadowsocks
+function install_ss(){
+    which pip > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        if [ "$OS" == 'CentOS' ]; then
+            python ez_setup.py install
+            easy_install pip
         fi
     fi
-    cd $cur_dir
-    # Delete shadowsocks-libev floder
-    rm -rf $cur_dir/shadowsocks-libev-master/
-    # Delete shadowsocks-libev zip file
-    rm -f shadowsocks-libev.zip
-    clear
-    echo ""
-    echo "Congratulations, shadowsocks-libev install completed!"
-    echo -e "Your Server IP: \033[41;37m ${IP} \033[0m"
-    echo -e "Your Server Port: \033[41;37m 8989 \033[0m"
-    echo -e "Your Password: \033[41;37m ${shadowsockspwd} \033[0m"
-    echo -e "Your Local IP: \033[41;37m 127.0.0.1 \033[0m"
-    echo -e "Your Local Port: \033[41;37m 1080 \033[0m"
-    echo -e "Your Encryption Method: \033[41;37m aes-256-cfb \033[0m"
-    echo ""
-    echo "Enjoy it!"
-    echo ""
-    exit 0
+    if [ -f /usr/bin/pip ]; then
+        pip install M2Crypto
+        pip install greenlet
+        pip install gevent
+        pip install shadowsocks
+        if [ -f /usr/bin/ssserver ] || [ -f /usr/local/bin/ssserver ]; then
+            chmod +x /etc/init.d/shadowsocks
+            # Add run on system start up
+            if [ "$OS" == 'CentOS' ]; then
+                chkconfig --add shadowsocks
+                chkconfig shadowsocks on
+            else
+                update-rc.d shadowsocks defaults
+            fi
+            # Run shadowsocks in the background
+            /etc/init.d/shadowsocks start
+        else
+            echo ""
+            echo "Shadowsocks install failed! Please visit http://teddysun.com/342.html and contact."
+            exit 1
+        fi
+        clear
+        echo ""
+        echo "Congratulations, shadowsocks install completed!"
+        echo -e "Your Server IP: \033[41;37m ${IP} \033[0m"
+        echo -e "Your Server Port: \033[41;37m 8989 \033[0m"
+        echo -e "Your Password: \033[41;37m ${shadowsockspwd} \033[0m"
+        echo -e "Your Local IP: \033[41;37m 127.0.0.1 \033[0m"
+        echo -e "Your Local Port: \033[41;37m 1080 \033[0m"
+        echo -e "Your Encryption Method: \033[41;37m aes-256-cfb \033[0m"
+        echo ""
+        echo "Welcome to visit:http://teddysun.com/342.html"
+        echo "Enjoy it!"
+        echo ""
+        exit 0
+    else
+        echo ""
+        echo "pip install failed! Please visit http://teddysun.com/342.html and contact."
+        exit 1
+    fi
 }
 
-# Uninstall Shadowsocks-libev
-function uninstall_shadowsocks_libev(){
-    printf "Are you sure uninstall shadowsocks_libev? (y/n) "
+# Uninstall Shadowsocks
+function uninstall_shadowsocks(){
+    printf "Are you sure uninstall Shadowsocks? (y/n) "
     printf "\n"
     read -p "(Default: n):" answer
     if [ -z $answer ]; then
         answer="n"
     fi
     if [ "$answer" = "y" ]; then
-        ps -ef | grep -v grep | grep -v ps | grep -i "ss-server" > /dev/null 2>&1
+        ps -ef | grep -v grep | grep -v ps | grep -i "ssserver" > /dev/null 2>&1
         if [ $? -eq 0 ]; then
             /etc/init.d/shadowsocks stop
         fi
-        chkconfig --del shadowsocks
+        checkos
+        if [ "$OS" == 'CentOS' ]; then
+            chkconfig --del shadowsocks
+        else
+            update-rc.d -f shadowsocks remove
+        fi
         # delete config file
-        rm -rf /etc/shadowsocks-libev
-        # delete shadowsocks
-        rm -f /usr/local/bin/ss-local
-        rm -f /usr/local/bin/ss-tunnel
-        rm -f /usr/local/bin/ss-server
-        rm -f /usr/local/bin/ss-redir
-        rm -f /usr/local/lib/libshadowsocks.a
-        rm -f /usr/local/lib/libshadowsocks.la
-        rm -f /usr/local/include/shadowsocks.h
-        rm -rf /usr/local/lib/pkgconfig
-        rm -f /usr/local/share/man/man8/shadowsocks.8
+        rm -f /etc/shadowsocks.json
+        rm -f /var/run/shadowsocks.pid
         rm -f /etc/init.d/shadowsocks
-        echo "Shadowsocks-libev uninstall success!"
+        pip uninstall -y shadowsocks
+        if [ $? -eq 0 ]; then
+            echo "Shadowsocks uninstall success!"
+        else
+            echo "Shadowsocks uninstall failed!"
+        fi
     else
         echo "uninstall cancelled, Nothing to do"
     fi
 }
 
-# Install Shadowsocks-libev
-function install_shadowsocks_libev(){
+# Install Shadowsocks-python
+function install_shadowsocks(){
+    checkos
     rootness
     disable_selinux
     pre_install
     download_files
     config_shadowsocks
-    if ! centosversion 7; then
-        iptables_set
+    if [ "$OS" == 'CentOS' ]; then
+        if ! centosversion 7; then
+            iptables_set
+        fi
     fi
-    install
+    install_ss
 }
 
 # Initialization step
@@ -253,10 +278,10 @@ action=$1
 [  -z $1 ] && action=install
 case "$action" in
 install)
-    install_shadowsocks_libev
+    install_shadowsocks
     ;;
 uninstall)
-    uninstall_shadowsocks_libev
+    uninstall_shadowsocks
     ;;
 *)
     echo "Arguments error! [${action} ]"
